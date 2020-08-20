260ba410484c4f1aca3d7860b8c8e81bc012f59f
Marcin K <marcin@kalik.dev>
2020-08-20 19:41:24 +0200

    Example for Reverse SSH Tunnel

---
+++ Examples/ReverseSSHTunnelExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using FieldBotNG;
+using FieldBotNG.Tools;
+using System;
+using System.Threading.Tasks;
+
+namespace ReverseSSHTunnelExample
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            ReverseSSHTunnel reverseSSHTunnel = new ReverseSSHTunnel(Helper.AppConfig.RemoteHost, Helper.AppConfig.LocalHost);
+            reverseSSHTunnel.Start();
+
+            TunnelConnectionState currentConnectionState = await reverseSSHTunnel.CheckConnectionType();
+            Console.WriteLine($"\nTunnel has been started. Current connection state => {currentConnectionState}");
+
+            Console.WriteLine("\nClick any key, to stop tunnel...");
+            Console.ReadKey();
+
+            currentConnectionState = await reverseSSHTunnel.Stop();
+            Console.WriteLine($"\nTunnel has been stopped. Current connection state => {currentConnectionState}");
+
+            Console.WriteLine("\nClick any key, to exit demo app...");
+            Console.ReadKey();
+        }
+    }
+}
