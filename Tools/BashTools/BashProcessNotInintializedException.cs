12106a7bf1851b500c1ef1b327dbec024640809e
Marcin K <marcin@kalik.dev>
2020-08-21 15:36:01 +0200

    New exceptions and documentation commentss

---
+++ Tools/BashTools/BashProcessNotInintializedException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BashTools
+{
+    /// <summary>
+    /// Represents error that occur when BashProcess is not initialized
+    /// </summary>
+    public class BashProcessNotInintializedException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of BashProcessNotInintializedException
+        /// </summary>
+        public BashProcessNotInintializedException()
+        {
+        }
+    }
+}
---
+++ Tools/BashTools/BashProcessNotRunningException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BashTools
+{
+    /// <summary>
+    /// Represents error that occur when BashProcess is not running and there was try to do something forbidden in this state
+    /// </summary>
+    public class BashProcessNotRunningException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of BashProcessNotRunningException
+        /// </summary>
+        public BashProcessNotRunningException()
+        {
+        }
+    }
+}
