d6ad3b449ee3c99763935518aeaac2e11bc8b855
Marcin <marcin@kalik.dev>
2021-10-11 23:45:10 +0200

    ExampleApp changed to works with new Tools version + new AppSettings class + appsettings.json adapted to changes

--- Examples/ReverseSSHTunnelExample/Program.cs
+++ Examples/ReverseSSHTunnelExample/Program.cs
@@ -10,7 +10,44 @@ namespace ReverseSSHTunnelExample
         {
             BashTools.BashProcess.IsWSL = SettingsManager.AppConfig.WSL;
 
-            ReverseSSHTunnel reverseSSHTunnel = new ReverseSSHTunnel(SettingsManager.AppConfig.RemoteHost, SettingsManager.AppConfig.LocalHost);
+            int hostsLength = SettingsManager.AppConfig.Hosts.Length;
+            if (hostsLength == 0)
+            {
+                Console.WriteLine("ERROR: There's no hosts in config file!!!");
+            }
+            else if (hostsLength == 1)
+            {
+                await CreateConnectionAsync();
+            }
+            else
+            {
+                Console.WriteLine($"There are {hostsLength} hosts - select correct index and press ENTER:");
+                for (int i = 0; i < hostsLength; i++)
+                {
+                    var hostsInfo = SettingsManager.AppConfig.Hosts[i];
+
+                    Console.WriteLine($"[{i}]: {hostsInfo.LocalHost} -> {hostsInfo.RemoteHost}");
+                }
+
+                string selectedIndex = Console.ReadLine();
+                bool isIntegerOK = int.TryParse(selectedIndex, out int index);
+                index = isIntegerOK ? index : hostsLength - 1;
+
+                Console.WriteLine($"Creating connection for [{index}]");
+
+                await CreateConnectionAsync(index);
+            }
+
+            Console.WriteLine("\nClick any key, to exit demo app...");
+            Console.ReadKey();
+        }
+
+        static async Task CreateConnectionAsync(int hostIndex = 0)
+        {
+            ReverseSSHTunnel reverseSSHTunnel = 
+                new ReverseSSHTunnel(SettingsManager.AppConfig.Hosts[hostIndex].RemoteHost, 
+                                     SettingsManager.AppConfig.Hosts[hostIndex].LocalHost);
+
             reverseSSHTunnel.Start();
 
             TunnelConnectionState currentConnectionState = await reverseSSHTunnel.CheckConnectionType();
@@ -19,11 +56,9 @@ namespace ReverseSSHTunnelExample
             Console.WriteLine("\nClick any key, to stop tunnel...");
             Console.ReadKey();
 
-            currentConnectionState = (await reverseSSHTunnel.Stop()).Item1;
+            currentConnectionState = (await reverseSSHTunnel.Stop()).TunnelConnectionState;
             Console.WriteLine($"\nTunnel has been stopped. Current connection state => {currentConnectionState}");
 
-            Console.WriteLine("\nClick any key, to exit demo app...");
-            Console.ReadKey();
         }
     }
 }
